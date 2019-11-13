6f652392a46f93a9dbc10b3e8347687b778f6106
kylefirst <kylefirst@gmail.com>
2019-11-13 12:27:08 -0500

    added empty template files for all activities

---
+++ app/src/main/java/com/example/feelingsdiary/NotificationTimeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.feelingsdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationTimeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_time)
+    }
+}
---
+++ app/src/main/java/com/example/feelingsdiary/ViewEntryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.feelingsdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewEntryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_entry)
+    }
+}

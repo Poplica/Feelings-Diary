71257b385a0530422801ac77be7d30528b909806
kylefirst <kylefirst@gmail.com>
2019-11-19 15:57:01 -0500

    added button behavior for settings screen

--- app/src/main/java/com/example/feelingsdiary/SettingsActivity.kt
+++ app/src/main/java/com/example/feelingsdiary/SettingsActivity.kt
@@ -1,12 +1,34 @@
 package com.example.feelingsdiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        supportActionBar!!.title = getString(R.string.settings)
+
+        findViewById<Button>(R.id.change_pass_button).setOnClickListener {
+            startActivity(
+                Intent(this@SettingsActivity, ChangePasswordActivity::class.java)
+            )
+        }
+
+        findViewById<Button>(R.id.change_frequency_button).setOnClickListener {
+            startActivity(
+                Intent(this@SettingsActivity, NotificationFrequencyActivity::class.java)
+            )
+        }
+
+        findViewById<Button>(R.id.change_notif_time_button).setOnClickListener {
+            startActivity(
+                Intent(this@SettingsActivity, NotificationTimeActivity::class.java)
+            )
+        }
     }
 }

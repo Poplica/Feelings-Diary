ab3b27cdf672d254530b6f725c77e1c29fee6fd1
kylefirst <kylefirst@gmail.com>
2019-11-19 16:06:27 -0500

    added saving notification preferences

--- app/src/main/java/com/example/feelingsdiary/NotificationFrequencyActivity.kt
+++ app/src/main/java/com/example/feelingsdiary/NotificationFrequencyActivity.kt
@@ -1,12 +1,40 @@
 package com.example.feelingsdiary
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 
 class NotificationFrequencyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification_frequency)
+
+        val prefs : SharedPreferences = getSharedPreferences("feelings_diary", Context.MODE_PRIVATE)
+
+        val mFrequencyGroup : RadioGroup = findViewById(R.id.frequency_group)
+        val mSaveButton : Button = findViewById(R.id.save_notification_frequency_btn)
+
+        mFrequencyGroup.check(mFrequencyGroup.getChildAt(prefs.getInt("notification_setting", 0)).id)
+
+        // save notification frequency button
+        mSaveButton.setOnClickListener {
+            val selected : RadioButton = findViewById(mFrequencyGroup.checkedRadioButtonId)
+
+            // store user selected frequency option
+            prefs.edit().putInt("notification_setting", mFrequencyGroup.indexOfChild(selected))
+                .apply()
+
+            Toast.makeText(
+                this@NotificationFrequencyActivity,
+                getString(R.string.saved),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/example/feelingsdiary/NotificationTimeActivity.kt
+++ app/src/main/java/com/example/feelingsdiary/NotificationTimeActivity.kt
@@ -1,12 +1,36 @@
 package com.example.feelingsdiary
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TimePicker
+import android.widget.Toast
 
 class NotificationTimeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification_time)
+
+        val prefs : SharedPreferences = getSharedPreferences("feelings_diary", MODE_PRIVATE)
+
+        val mTimePicker : TimePicker = findViewById(R.id.notification_time)
+        val mSaveButton : Button = findViewById(R.id.save_notification_time_btn)
+
+        // save notification time button
+        mSaveButton.setOnClickListener {
+            val hour = (mTimePicker.hour * 3600000).toLong()
+            val minute = (mTimePicker.minute * 60000).toLong()
+
+            // store user selected time option
+            prefs.edit().putLong("notification_time", hour + minute).apply()
+
+            Toast.makeText(
+                this@NotificationTimeActivity,
+                getString(R.string.restart_app),
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }

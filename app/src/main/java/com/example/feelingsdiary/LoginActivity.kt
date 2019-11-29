37efaf09b6537529ec8a53817f0439d6fbc5727e
Poplica <tu9489@gmail.com>
2019-11-29 16:32:50 -0500

    added firebase to project and implemented login screen activity

--- app/src/main/java/com/example/feelingsdiary/LoginActivity.kt
+++ app/src/main/java/com/example/feelingsdiary/LoginActivity.kt
@@ -3,18 +3,28 @@ package com.example.feelingsdiary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private var viewEmail: TextView? = null
+    private var viewPassword: EditText? = null
+    private var progressBar: ProgressBar? = null
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val viewEmail = findViewById<View>(R.id.email)
-        val viewPassword = findViewById<View>(R.id.password)
-        val viewLoginForm = findViewById<View>(R.id.email_login_form)
-        val viewProgress = findViewById<View>(R.id.login_progress)
+        viewEmail = findViewById<View>(R.id.email) as TextView
+        viewPassword = findViewById<View>(R.id.password) as EditText
+        progressBar = findViewById<View>(R.id.login_progress) as ProgressBar
+        mAuth = FirebaseAuth.getInstance()
 
         val viewSignUp = findViewById<View>(R.id.sign_up_link)
         viewSignUp.setOnClickListener {
@@ -28,7 +38,43 @@ class LoginActivity : AppCompatActivity() {
 
         val buttonLogin = findViewById<View>(R.id.log_in_button)
         buttonLogin.setOnClickListener {
-
+            loginUserAccount()
         }
+    }
+
+    private fun loginUserAccount() {
+        progressBar!!.visibility = View.VISIBLE
+
+//        onBackPressed()
+        val email: String = viewEmail!!.text.toString()
+        val password: String = viewPassword!!.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.signInWithEmailAndPassword(email, password).addOnCompleteListener {task ->
+            progressBar!!.visibility = View.GONE
+            if (task.isSuccessful) {
+                Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG).show()
+
+                val intent = Intent(applicationContext, HomeActivity::class.java)
+                intent.putExtra(UserMail, email)
+                intent.putExtra(UserID, mAuth!!.currentUser!!.uid)
+                startActivity(intent)
+            } else {
+                Toast.makeText(applicationContext,"Login failed! Please try again later", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    companion object {
+        val UserMail = "com.example.tesla.myhomelibrary.UMail"
+        val UserID = "com.example.tesla.myhomelibrary.UID"
     }
 }

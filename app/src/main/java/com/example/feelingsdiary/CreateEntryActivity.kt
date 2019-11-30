2fa8d996351b29adff719f3ce53fce879bd9d876
Mark Kap <kapski@terpmail.umd.edu>
2019-11-30 16:39:40 -0500

    spinner add
    Adding spinner for emotions
    and code in onCreate()

--- app/src/main/java/com/example/feelingsdiary/CreateEntryActivity.kt
+++ app/src/main/java/com/example/feelingsdiary/CreateEntryActivity.kt
@@ -2,11 +2,25 @@ package com.example.feelingsdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.R
+import android.widget.Spinner
+
+
 
 class CreateEntryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_entry)
+
+        val arraySpinner = arrayOf("Sad", "Depressed", "Angry", "Frustrated")
+        val s = findViewById(R.id.emotionSpinner) as Spinner
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.simple_spinner_item, arraySpinner
+        )
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        s.adapter = adapter
     }
 }
